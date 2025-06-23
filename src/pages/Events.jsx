6e4d6fd71f94b7d90bc7e4ca7c0a3059f16b2e1b
Juliet <hrishakovayuliia@gmail.com>
2025-06-23 11:54:56 +0200

    Fix fetch issue in Events.jsx

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,4 +1,34 @@
+import { useState, useEffect } from "react";
+
 function Events() {
-  return <div>Events</div>;
+  const [events, setEvents] = useState([
+    {id: 1, title: "Event 1", description: "Description 1"},
+    {id: 2, title: "Event 2", description: "Description 2"},
+    {id: 3, title: "Event 3", description: "Description 3"},
+  ]);
+
+  console.log(events);
+
+  useEffect(() => {
+    fetch('http://localhost:5173/api/events')
+    .then((response) => response.json())
+    .then((data) => {
+      setEvents(data.results);
+    })
+    .catch((err) => {
+      console.log(err);
+     });
+    }, []);
+
+    
+
+  return <div>EventsList:
+    {events.map((event) => {
+      return <div key={event.id}>
+        <p>Title: {event.title}</p>
+        <p>Title: {event.description}</p>
+      </div>
+    })}
+  </div>;
 }
 export default Events
